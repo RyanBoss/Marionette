14779bbfc6209095cb31e2b023918fc4a9e2b5a1
raboss <ryan.boss@iqmetrix.com>
2014-04-01 10:22:16 -0600

    added nancy test application to test getting website working

---
+++ NancyTest/NancyTest/Global.asax.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace NancyTest
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //ignore aspx pages (web forms take care of these)
+            routes.IgnoreRoute("{resource}.aspx/{*pathInfo}");
+
+            routes.IgnoreRoute("services/{*route}");
+            routes.MapRoute("Default", "{controller}/{action}/{id}",
+                            new {controller = "default", action = "index", id = ""});
+        }
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
